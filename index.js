5e17a1a01addf41ec2b4a6bbedfc91a80a33a964
EnricoAccelist <enrico.giovanno@project.accelist.com>
2023-01-16 16:17:42 +0700

    Add Animations

--- index.js
+++ index.js
@@ -4,40 +4,50 @@ const makeSound = (key) => {
             const audio1 = new Audio('./sounds/tom-1.mp3');
             audio1.play();
             break;
-    
+
         case 'a':
             const audio2 = new Audio('./sounds/tom-2.mp3');
             audio2.play();
             break;
-    
+
         case 's':
             const audio3 = new Audio('./sounds/tom-3.mp3');
             audio3.play();
             break;
-    
+
         case 'd':
             const audio4 = new Audio('./sounds/tom-4.mp3');
             audio4.play();
             break;
-    
+
         case 'j':
             const audio5 = new Audio('./sounds/snare.mp3');
             audio5.play();
             break;
-    
+
         case 'k':
             const audio6 = new Audio('./sounds/crash.mp3');
             audio6.play();
             break;
-    
+
         case 'l':
             const audio7 = new Audio('./sounds/kick-bass.mp3');
             audio7.play();
             break;
-    
+
         default:
-            console.log(key);
             break;
+    }
+}
+
+const buttonAnimation = (key) => {
+    const selected = document.querySelector('.' + key)
+
+    if (selected) {
+        selected.classList.add('pressed');
+        setTimeout(() => {
+            document.querySelector('.' + key).classList.remove('pressed');
+        }, 100)
     }
 }
 
@@ -47,9 +57,11 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', () => {
         const innerHTML = this.document.querySelectorAll('.drum')[i].innerHTML;
         makeSound(innerHTML);
+        buttonAnimation(innerHTML);
     });
 };
 
 document.addEventListener('keydown', (event) => {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
