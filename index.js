d2b11011e2088afb0461c8e2767b9dad65a335d3
EnricoAccelist <enrico.giovanno@project.accelist.com>
2023-01-16 15:56:08 +0700

    Add keydown event listener

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const audios = [
-    new Audio('./sounds/tom-1.mp3'),
-    new Audio('./sounds/tom-2.mp3'),
-    new Audio('./sounds/tom-3.mp3'),
-    new Audio('./sounds/tom-4.mp3'),
-    new Audio('./sounds/snare.mp3'),
-    new Audio('./sounds/crash.mp3'),
-    new Audio('./sounds/kick-bass.mp3'),
-];
-
-const handleClick = (index) => {
-    return audios[index].play();
-};
+const makeSound = (key) => {
+    switch (key) {
+        case 'w':
+            const audio1 = new Audio('./sounds/tom-1.mp3');
+            audio1.play();
+            break;
+    
+        case 'a':
+            const audio2 = new Audio('./sounds/tom-2.mp3');
+            audio2.play();
+            break;
+    
+        case 's':
+            const audio3 = new Audio('./sounds/tom-3.mp3');
+            audio3.play();
+            break;
+    
+        case 'd':
+            const audio4 = new Audio('./sounds/tom-4.mp3');
+            audio4.play();
+            break;
+    
+        case 'j':
+            const audio5 = new Audio('./sounds/snare.mp3');
+            audio5.play();
+            break;
+    
+        case 'k':
+            const audio6 = new Audio('./sounds/crash.mp3');
+            audio6.play();
+            break;
+    
+        case 'l':
+            const audio7 = new Audio('./sounds/kick-bass.mp3');
+            audio7.play();
+            break;
+    
+        default:
+            console.log(key);
+            break;
+    }
+}
 
 const numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll('.drum')[i].addEventListener('click', () => handleClick(i));
-}
+    document.querySelectorAll('.drum')[i].addEventListener('click', () => {
+        const innerHTML = this.document.querySelectorAll('.drum')[i].innerHTML;
+        makeSound(innerHTML);
+    });
+};
+
+document.addEventListener('keydown', (event) => {
+    makeSound(event.key);
+});
